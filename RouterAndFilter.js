ded66c86a71b21ee3f82e1606c2915984d4d9e2c
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-03-10 21:38:11 +0530

    Add files via upload

---
+++ RouterAndFilter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRouter = require('./Routes/admin');
+const shopRouter = require('./Routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// filtering => /admin.
+app.use('/admin',adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000);
